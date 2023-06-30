53026044da5aeddbc7616a55f6127aacf72c820c
罗庚中 <huanggengzhong@daishujiankang.com>
2023-06-30 15:40:52 +0800

    feat:开始文章

---
+++ routers/api/v1/article.go
@@ -0,0 +1,19 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func GetArtical(c *gin.Context) {
+
+}
+func GetArticals(c *gin.Context) {
+
+}
+func AddArtical(c *gin.Context) {
+
+}
+func EditArtical(c *gin.Context) {
+
+}
+func DeleteArtical(c *gin.Context) {
+
+}
--- routers/router.go
+++ routers/router.go
@@ -20,10 +20,16 @@ func InitRouter() *gin.Engine {
 	// 路由分组
 	apiv1 := r.Group("/api/v1")
 	{
+		// 标签路由组
 		apiv1.GET("/tags", v1.GetTags)
 		apiv1.POST("/tags", v1.AddTag)
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		// 文章路由组
+		apiv1.GET("/articals", v1.GetArtical)
+		apiv1.POST("/articals", v1.AddArtical)
+		apiv1.PUT("/articals/:id", v1.EditArtical)
+		apiv1.DELETE("/articals/:id", v1.DeleteArtical)
 	}
 	return r
 }
