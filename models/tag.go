bbec8cf46dbf7db5eef212fad739fce4b0f1c17f
罗庚中 <huanggengzhong@daishujiankang.com>
2023-06-30 11:52:41 +0800

    feat:新增标签调通

--- models/tag.go
+++ models/tag.go
@@ -1,23 +1,10 @@
-// package models
-
-// type Tag struct {
-// 	Model
-// 	Name       string `json:"name"`
-// 	CreateBy   string `json:created_by`
-// 	ModifiedBy string `json:"modified_by"`
-// 	State      int    `json:"state"`
-// }
-
-// func GetTags(pageNum int, pageSize int, maps interface{}) (tags Tag) {
-// 	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
-// 	return
-// }
-
-//	func GetTagTotal(maps interface{}) (count int) {
-//		db.Model(&Tag{}).Where(maps).Count(&count)
-//		return
-//	}
 package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Tag struct {
 	Model
@@ -38,4 +25,40 @@ func GetTagTotal(maps interface{}) (count int) {
 	db.Model(&Tag{}).Where(maps).Count(&count)
 
 	return
+}
+
+func ExistTagByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name= ?", name).Find(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+	return false
+}
+
+func AddTag(name string, state int, createdBy string) bool {
+
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	})
+	return true
+}
+
+// gorm所支持的自动回调方法：
+
+// 创建：BeforeSave、BeforeCreate、AfterCreate、AfterSave
+// 更新：BeforeSave、BeforeUpdate、AfterUpdate、AfterSave
+// 删除：BeforeDelete、AfterDelete
+// 查询：AfterFind
+
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+	return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+	return nil
 }
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"net/http"
 
+	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"github.com/huanggengzhong/go-gin-example/models"
 	"github.com/huanggengzhong/go-gin-example/pkg/e"
@@ -37,6 +38,31 @@ func GetTags(c *gin.Context) {
 	})
 }
 func AddTag(c *gin.Context) {
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
+	//校验
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("名称不能为空")
+	valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许0和1")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为100字符")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if !models.ExistTagByName(name) {
+			code = e.SUCCESS
+			models.AddTag(name, state, createdBy)
+		} else {
+			code = e.ERROR_EXIST_TAG
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 
 }
 func EditTag(c *gin.Context) {
